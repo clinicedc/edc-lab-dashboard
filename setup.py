2b1e641b693bb2e7a1cd25b35e4189b6c299a55c
Itumeleng Mosweu <imosweu@nkange.bhp.org.bw>
2017-06-07 15:56:21 +0200

    fix setup to reference edc-lab-dashboard

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='edc-lab',
+    name='edc-lab-dashboard',
     version='0.1.1',
     author=u'Erik van Widenfelt',
     author_email='ew2789@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/botswana-harvard/edc-lab',
+    url='https://github.com/botswana-harvard/edc-lab-dashboard',
     license='GPL license, see LICENSE',
     description='Edc old lab classes',
     long_description=README,
