74eb7e7e1f85cb6f3e48df254359e9e67c492619
erikvw <ew2789@gmail.com>
2018-07-22 15:34:01 +0200

    check for null context in middleware

--- edc_lab_dashboard/middleware.py
+++ edc_lab_dashboard/middleware.py
@@ -28,9 +28,10 @@ class DashboardMiddleware:
         request.template_data.update(**template_data)
 
     def process_template_response(self, request, response):
-        response.context_data.update(**request.url_name_data)
-        response.context_data.update(**request.template_data)
-        response.context_data.update(
-            SHIPPED=SHIPPED,
-        )
+        if response.context_data:
+            response.context_data.update(**request.url_name_data)
+            response.context_data.update(**request.template_data)
+            response.context_data.update(
+                SHIPPED=SHIPPED,
+            )
         return response
