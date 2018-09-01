753b41a04642f2e4e8dc698676237b3a83dd68d9
erikvw <ew2789@gmail.com>
2018-09-01 11:53:04 -0500

    add bootstrap to path

--- edc_lab_dashboard/middleware.py
+++ edc_lab_dashboard/middleware.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from edc_dashboard import insert_bootstrap_version
 from edc_lab.constants import SHIPPED
 
 from .dashboard_templates import dashboard_templates
@@ -15,16 +16,18 @@ class DashboardMiddleware:
 
     def process_view(self, request, *args):
         request.url_name_data.update(**dashboard_urls)
-        request.template_data.update(**dashboard_templates)
         try:
             url_name_data = settings.LAB_DASHBOARD_URL_NAMES
         except AttributeError:
             url_name_data = {}
-        try:
-            template_data = settings.LAB_DASHBOARD_BASE_TEMPLATES
-        except AttributeError:
-            template_data = {}
         request.url_name_data.update(**url_name_data)
+
+        template_data = dashboard_templates
+        try:
+            template_data.update(settings.LAB_DASHBOARD_BASE_TEMPLATES)
+        except AttributeError:
+            pass
+        template_data = insert_bootstrap_version(**template_data)
         request.template_data.update(**template_data)
 
     def process_template_response(self, request, response):
